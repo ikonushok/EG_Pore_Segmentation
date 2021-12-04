6ced7a422e6dff07e2bffc32abc3511bd47387a7
i.koniushok <i.koniushok@acti.ru>
2021-12-04 18:49:54 +0300

    добавил файлы

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,12 @@
 
 
 # https://drive.google.com/drive/folders/1Xy5Zs9kiFAVhRgmVkZrvgjjLU4B3ln4J?usp=sharing
+
+
+import gdown
+
+url = 'https://drive.google.com/drive/folders/1artmAF37Ms0yUZHDddqXiJF39T4SSaHi'
+gdown.download_folder(url, quiet=True,
+                      use_cookies=True,
+                      remaining_ok=True)
+
